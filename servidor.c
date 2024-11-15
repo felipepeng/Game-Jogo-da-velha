7d41712148a2160ce0dc2732361af883d24c5fcc
Peng <felipebarretocortes.fb@gmail.com>
2024-11-15 18:49:19 -0300

    sistema aleatório de palavras

--- servidor.c
+++ servidor.c
@@ -3,6 +3,39 @@
 #include <stdlib.h>
 #include <winsock2.h>
 
+void escolherPalavra(char *palavraEscolhida) {
+    // Palavras disponíveis
+    const char *palavras[] = {
+        "programacao",
+        "computador",
+        "engenharia",
+        "software",
+        "dados",
+        "ciencia",
+        "internet",
+        "vpn"
+    };
+    
+    // Total de palavras no array
+    int totalPalavras = sizeof(palavras) / sizeof(palavras[0]);
+    
+    // Inicializar o gerador de números aleatórios
+    srand(time(NULL));
+
+    // Escolher uma palavra aleatória
+    int indiceAleatorio = rand() % totalPalavras;
+
+    // Copiar a palavra escolhida para a variável de saída
+    strcpy(palavraEscolhida, palavras[indiceAleatorio]);
+}
+
 int main() {
-	
+    char palavra[50];
+
+    // Escolher uma palavra aleatória
+    escolherPalavra(palavra);
+
+    printf("A palavra escolhida aleatoriamente foi: %s\n", palavra);
+
+    return 0;
 }
